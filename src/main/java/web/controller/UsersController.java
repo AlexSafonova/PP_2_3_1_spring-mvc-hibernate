8d3b02a5ff1a2aa5f3f7f541a0ad5aaad3db3fff
alexandra <alexsafonova1092@gmail.com>
2023-12-15 15:19:38 +0500

    Raw

--- src/main/java/web/controller/UsersController.java
+++ src/main/java/web/controller/UsersController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.User;
 import web.service.UserService;
@@ -26,5 +27,24 @@ public class UsersController {
         model.addAttribute("users", userService.getAllUsers());
         return "users";
     }
+    @PostMapping(value = "/add")
+    public String addUser(@RequestParam String name, @RequestParam String surname) {
+        User user = new User(name, surname);
+        userService.addUser(user);
+        return "redirect:/";
+    }
+    @PostMapping(value = "/update")
+    public String updateUser(@RequestParam Long id, @RequestParam String name, @RequestParam String surname) {
+        User user = new User(name, surname);
+        user.setId(id);
+        userService.updateUser(user);
+        return "redirect:/";
+    }
+    @PostMapping(value = "/delete")
+    public String deleteUser(@RequestParam Long id) {
+        userService.deleteUser(id);
+        return "redirect:/";
+    }
+
 
 }
